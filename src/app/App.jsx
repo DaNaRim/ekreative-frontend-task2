a0af6e77a20acbffbea736f9047f59e191392535
Nazar Horshevikov <rangar.danarim@gmail.com>
2023-02-03 21:31:39 +0200

    ADD ComingSoonPage

--- src/app/App.jsx
+++ src/app/App.jsx
@@ -2,6 +2,7 @@ import React from "react";
 import {Route, Routes} from "react-router";
 import PageWrapper from "../common/components/base/PageWrapper/PageWrapper";
 import AboutUsPage from "../common/pages/AboutUsPage/AboutUsPage";
+import ComingSoonPage from "../common/pages/ComingSoonPage/ComingSoonPage";
 import HomePage from "../common/pages/HomePage/HomePage";
 import PricingPage from "../common/pages/PricingPage/PricingPage";
 import "./App.scss";
@@ -12,7 +13,7 @@ const App = () => (
       <Route path="/" index element={<HomePage/>}/>
       <Route path="/aboutUs" element={<AboutUsPage/>}/>
       <Route path="/pricing" element={<PricingPage/>}/>
-      <Route path="*" element={<HomePage/>}/>
+      <Route path="*" element={<ComingSoonPage/>}/>
     </Route>
   </Routes>
 );
