916733eec7e91d8ce77e1f0cdb283312ca3e71f7
Nazar Horshevikov <rangar.danarim@gmail.com>
2023-02-03 16:11:44 +0200

    ADD OurTeam section in AboutUsPage.

--- src/common/pages/AboutUsPage/AboutUsPage.jsx
+++ src/common/pages/AboutUsPage/AboutUsPage.jsx
@@ -2,105 +2,18 @@ import AboutUsHeader from "../../components/sections/AboutUsHeader/AboutUsHeader
 import MissionAndVision from "../../components/sections/MissionAndVision/MissionAndVision";
 import OurBenefits from "../../components/sections/OurBenefits/OurBenefits";
 import OurProcess from "../../components/sections/OurProcess/OurProcess";
+import OurTeam from "../../components/sections/OurTeam/OurTeam";
 import WhoWeAre from "../../components/sections/WhoWeAre/WhoWeAre";
 
-const AboutUsPage = () => {
-  return (
-    <main>
-      <AboutUsHeader/>
-      <WhoWeAre/>
-      <OurProcess/>
-      <MissionAndVision/>
-      <OurBenefits/>
-
-
-      {/*<section className="our_team bgc_grey col_dark_blue">*/}
-      {/*  <h2>Meet our team</h2>*/}
-      {/*  <div className="card_wrapper">*/}
-      {/*    <div className="card">*/}
-      {/*      <div className="image_wrapper">*/}
-      {/*        <img src="../img/team_John_Smith.png" alt="John Smith" height="336" width="336"/>*/}
-      {/*        <div className="image_overlay">*/}
-      {/*          <div className="socials">*/}
-      {/*            <a href="https://www.facebook.com/" target="_blank">*/}
-      {/*              <i className="fa-brands fa-facebook"></i>*/}
-      {/*            </a>*/}
-      {/*            <a href="https://twitter.com/" target="_blank">*/}
-      {/*              <i className="fa-brands fa-twitter"></i>*/}
-      {/*            </a>*/}
-      {/*            <a href="https://www.linkedin.com/" target="_blank">*/}
-      {/*              <i className="fa-brands fa-linkedin"></i>*/}
-      {/*            </a>*/}
-      {/*          </div>*/}
-      {/*        </div>*/}
-      {/*      </div>*/}
-      {/*      <h6>John Smith</h6>*/}
-      {/*      <p className="body1">CEO</p>*/}
-      {/*    </div>*/}
-      {/*    <div className="card">*/}
-      {/*      <div className="image_wrapper">*/}
-      {/*        <img src="../img/team_Simon_Adams.png" alt="Simon Adams" height="336" width="336"/>*/}
-      {/*        <div className="image_overlay">*/}
-      {/*          <div className="socials">*/}
-      {/*            <a href="https://www.facebook.com/" target="_blank">*/}
-      {/*              <i className="fa-brands fa-facebook"></i>*/}
-      {/*            </a>*/}
-      {/*            <a href="https://twitter.com/" target="_blank">*/}
-      {/*              <i className="fa-brands fa-twitter"></i>*/}
-      {/*            </a>*/}
-      {/*            <a href="https://www.linkedin.com/" target="_blank">*/}
-      {/*              <i className="fa-brands fa-linkedin"></i>*/}
-      {/*            </a>*/}
-      {/*          </div>*/}
-      {/*        </div>*/}
-      {/*      </div>*/}
-      {/*      <h6>Simon Adams</h6>*/}
-      {/*      <p className="body1">CTO</p>*/}
-      {/*    </div>*/}
-      {/*    <div className="card">*/}
-      {/*      <div className="image_wrapper">*/}
-      {/*        <img src="../img/team_Paul_Jones.png" alt="Paul Jones" height="336" width="336"/>*/}
-      {/*        <div className="image_overlay">*/}
-      {/*          <div className="socials">*/}
-      {/*            <a href="https://www.facebook.com/" target="_blank">*/}
-      {/*              <i className="fa-brands fa-facebook"></i>*/}
-      {/*            </a>*/}
-      {/*            <a href="https://twitter.com/" target="_blank">*/}
-      {/*              <i className="fa-brands fa-twitter"></i>*/}
-      {/*            </a>*/}
-      {/*            <a href="https://www.linkedin.com/" target="_blank">*/}
-      {/*              <i className="fa-brands fa-linkedin"></i>*/}
-      {/*            </a>*/}
-      {/*          </div>*/}
-      {/*        </div>*/}
-      {/*      </div>*/}
-      {/*      <h6>Paul Jones</h6>*/}
-      {/*      <p className="body1">Design Lead</p>*/}
-      {/*    </div>*/}
-      {/*    <div className="card">*/}
-      {/*      <div className="image_wrapper">*/}
-      {/*        <img src="../img/team_Sara_Hardin.png" alt="Sara Hardin" height="336" width="336"/>*/}
-      {/*        <div className="image_overlay">*/}
-      {/*          <div className="socials">*/}
-      {/*            <a href="https://www.facebook.com/" target="_blank">*/}
-      {/*              <i className="fa-brands fa-facebook"></i>*/}
-      {/*            </a>*/}
-      {/*            <a href="https://twitter.com/" target="_blank">*/}
-      {/*              <i className="fa-brands fa-twitter"></i>*/}
-      {/*            </a>*/}
-      {/*            <a href="https://www.linkedin.com/" target="_blank">*/}
-      {/*              <i className="fa-brands fa-linkedin"></i>*/}
-      {/*            </a>*/}
-      {/*          </div>*/}
-      {/*        </div>*/}
-      {/*      </div>*/}
-      {/*      <h6>Sara Hardin</h6>*/}
-      {/*      <p className="body1">Project Manager</p>*/}
-      {/*    </div>*/}
-      {/*  </div>*/}
-      {/*</section>*/}
-    </main>
-  );
-};
+const AboutUsPage = () => (
+  <main>
+    <AboutUsHeader/>
+    <WhoWeAre/>
+    <OurProcess/>
+    <MissionAndVision/>
+    <OurBenefits/>
+    <OurTeam/>
+  </main>
+);
 
 export default AboutUsPage;
