3ba8b42ba5421bf263638adb30e18c02c5fc2eab
Nazar Horshevikov <rangar.danarim@gmail.com>
2023-02-03 22:00:06 +0200

    ADD lazy loading to pages

--- src/app/App.jsx
+++ src/app/App.jsx
@@ -1,11 +1,12 @@
 import React from "react";
 import {Route, Routes} from "react-router";
 import PageWrapper from "../common/components/base/PageWrapper/PageWrapper";
-import AboutUsPage from "../common/pages/AboutUsPage/AboutUsPage";
-import ComingSoonPage from "../common/pages/ComingSoonPage/ComingSoonPage";
-import HomePage from "../common/pages/HomePage/HomePage";
-import PricingPage from "../common/pages/PricingPage/PricingPage";
 import "./App.scss";
+
+const HomePage = React.lazy(() => import("../common/pages/HomePage/HomePage"));
+const AboutUsPage = React.lazy(() => import("../common/pages/AboutUsPage/AboutUsPage"));
+const PricingPage = React.lazy(() => import("../common/pages/PricingPage/PricingPage"));
+const ComingSoonPage = React.lazy(() => import("../common/pages/ComingSoonPage/ComingSoonPage"));
 
 const App = () => (
   <Routes>
--- src/common/components/base/PageWrapper/PageWrapper.jsx
+++ src/common/components/base/PageWrapper/PageWrapper.jsx
@@ -1,13 +1,17 @@
+import React, {Suspense} from "react";
 import {Outlet} from "react-router";
 import Footer from "../Footer/Footer";
 import Header from "../Header/Header";
+import Loading from "../Loading/Loading";
 
 import styles from "./PageWrapper.module.scss";
 
 const PageWrapper = () => (
   <div className={styles.page_wrapper}>
     <Header className={styles.main_header}/>
-    <Outlet/>
+    <Suspense fallback={<Loading/>}>
+      <Outlet/>
+    </Suspense>
     <Footer className={styles.main_footer}/>
   </div>
 );
